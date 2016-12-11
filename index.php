f5498ed34c0564acfc94aa9ef2af4d2facca9550
votoms <leo0210leo@yahoo.co.jp>
2016-12-12 01:00:50 +0900

    modify

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
 <?php
+$accessToken = 'NcNXkgzBiEABNxdNOk9mGVv2IBL/04HevKtJfozIRYhxFE9OMFDrc8GYoLiDe++VIzUSIvpjNyE9RxKqqgOFjD4kTaR7EjrXK7g3B5vr7JFeFOmAusk7IdvNiLfPthizidIfbfEl0MT9fHxpIZHzfgdB04t89/1O/w1cDnyilFU=';
+
+
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$jsonObj = json_decode($json_string);
+
+$type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
+//メッセージ取得
+$text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
+//ReplyToken取得
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+
+//メッセージ以外のときは何も返さず終了
+if($type != "text"){
+	exit;
+}
+
+//返信データ作成
+$response_format_text = [
+	"type" => "text",
+	"text" => "金剛デース！"
+	];
+$post_data = [
+	"replyToken" => $replyToken,
+	"messages" => [$response_format_text]
+	];
+
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json; charser=UTF-8',
+    'Authorization: Bearer ' . $accessToken
+    ));
+$result = curl_exec($ch);
+curl_close($ch);
+
 /*
  * 環境変数として以下を使用しています
  * - LINE_CHANNEL_ID
@@ -7,7 +47,7 @@
  * - FIXIE_URL
  * - APP_NAME
  */
-
+/*
 error_log("START: PHP");
 
 $phpInput = json_decode(file_get_contents('php://input'));
@@ -58,3 +98,4 @@ function createTextResponse($message) {
 function createImageResponse($imageUrl, $thumbnailImageUrl) {
     return ['contentType' => 2, "toType" => 1, 'originalContentUrl' => $imageUrl, "previewImageUrl" => $thumbnailImageUrl];
 }
+*/
