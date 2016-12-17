fc18ef3784fcff53f6267ef017caf3cca6a23f34
votoms <leo0210leo@yahoo.co.jp>
2016-12-17 16:18:19 +0900

    modify

--- index.php
+++ index.php
@@ -195,13 +195,13 @@ function makeButtonTemplateData($length) {
     ];
 }
 
-{
-    "type": "location",
-    "title": "my location",
-    "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-    "latitude": 35.65910807942215,
-    "longitude": 139.70372892916203
-}
+// {
+//     "type": "location",
+//     "title": "my location",
+//     "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+//     "latitude": 35.65910807942215,
+//     "longitude": 139.70372892916203
+// }
 
 /*
  * 環境変数として以下を使用しています
