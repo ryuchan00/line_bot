79d932c45300c2673ac6ac454badb0408a1c4a5e
votoms <leo0210leo@yahoo.co.jp>
2016-12-22 21:49:35 +0900

    modify

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ .'/vender/autoload.php';
+var_dump(__DIR__ .'/vender/autoload.php');
 
 $httpClient = new
 \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
@@ -18,7 +19,6 @@ try {
 } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
   error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
 }
-
 foreach ($events as $event) {
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
     error_log('Non message event has come');
