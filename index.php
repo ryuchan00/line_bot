0f3ab8d1b07da0e404743ffe4514072f61023c03
votoms <leo0210leo@yahoo.co.jp>
2016-12-23 10:41:31 +0900

    modify

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ .'/vender/autoload.php';
+echo "test";
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$httpClient = new
+\LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
@@ -17,7 +19,6 @@ try {
 } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
   error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
 }
-
 foreach ($events as $event) {
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
     error_log('Non message event has come');
@@ -30,4 +31,4 @@ foreach ($events as $event) {
   $bot->replyText($event->getReplyToken(), $event->getText());
 }
 
- ?>
+?>
