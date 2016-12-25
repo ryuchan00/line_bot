4d4d1605f39c358149848560acaf8bc8a8f656b4
yamakawa00 <leokun0210@gmail.com>
2016-12-25 21:43:33 +0900

    modify

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
-require_once __DIR__ .'/vender/autoload.php';
-echo "test";
+require_once __DIR__ . '/vendor/autoload.php';
 
-$httpClient = new
-\LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
@@ -19,6 +17,7 @@ try {
 } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
   error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
 }
+
 foreach ($events as $event) {
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
     error_log('Non message event has come');
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit8cbedb473a6a043286a9ebe4276a35e5
+class ComposerStaticInit54ded87bea2255daa78ec33b17bef387
 {
     public static $prefixLengthsPsr4 = array (
         'L' => 
@@ -23,8 +23,8 @@ class ComposerStaticInit8cbedb473a6a043286a9ebe4276a35e5
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit8cbedb473a6a043286a9ebe4276a35e5::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit8cbedb473a6a043286a9ebe4276a35e5::$prefixDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit54ded87bea2255daa78ec33b17bef387::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit54ded87bea2255daa78ec33b17bef387::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
