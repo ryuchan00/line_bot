a1bac01015eb078c1cffd8b0b6519e3b514c88d1
yamakawa00 <leokun0210@gmail.com>
2016-12-28 22:10:39 +0900

    modify

--- db_connect.php
+++ db_connect.php
@@ -11,3 +11,4 @@ $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1))
 
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
 var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+
