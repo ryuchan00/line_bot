c9b45a742bf561234c4533b6ffb65d6f587ffc71
yamakawa <yamakawa@cbase.co.jp>
2017-01-17 20:01:01 +0900

    update

--- index.php
+++ index.php
@@ -56,6 +56,7 @@ $pdo = new PDO($dsn, $url['user'], $url['pass']);
 
 $sql = 'insert into public.user (user_line_id, name, comment, picture_url) values (?, ?, ?, ?)';
 $stmt = $pdo->prepare($sql);
+error_log($user_info);
 $flag = $stmt->execute(array($user_info));
 
 //if ($flag){
