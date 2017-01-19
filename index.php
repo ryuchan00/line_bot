01ae6c7fae8ba1a59ac3c28013bb3365ebdd5122
yamakawa00 <leokun0210@gmail.com>
2017-01-19 20:03:39 +0900

    up]

--- index.php
+++ index.php
@@ -133,19 +133,19 @@ function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUr
     }
 }
 
-function replyButtonsTemplate($bot, $replyToken, $alternativeText, $imageUrl, $title, $text, $actions) {
-    $actionArray = array();
-    foreach($actions as $value) {
-        array_push($actionArray, $value);
-    }
-    $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
-        $alternativeText,
-        new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder ($title, $text, $imageUrl, $actionArray)
-    );
-    $response = $bot->replyMessage($replyToken, $builder);
-    if (!$response->isSucceeded()) {
-        error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
-    }
+function replyButtonsTemplate($bot, $replyToken, $alternativeText, $imageUrl, $title, $text, ...$actions) {
+  $actionArray = array();
+  foreach($actions as $value) {
+    array_push($actionArray, $value);
+  }
+  $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
+    $alternativeText,
+    new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder ($title, $text, $imageUrl, $actionArray)
+  );
+  $response = $bot->replyMessage($replyToken, $builder);
+  if (!$response->isSucceeded()) {
+    error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
 }
 
 function replyCarouselTemplate($bot, $replyToken, $alternativeText, $columnArray) {
