eba7fd5f5bb124abbe5c315bade54070d6187911
yamakawa00 <leokun0210@gmail.com>
2017-02-03 18:35:57 +0900

    up

---
+++ get.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: CBASE86
+ * Date: 2017/02/03
+ * Time: 17:55
+ */
+
+// リクエストメソッド取得
+$request_method = get_request_method();
+
+if ($request_method === 'GET') {
+    foreach ($_GET as $k => $v) {
+        if ($k == "PIC") {
+            echo '<img src="' . $v . '"">';
+        }
+        echo "受信パラメータ" . $k . "：" . $v . "</br>";
+    }
+}
+
+/**
+ * リクエストメソッドを取得
+ * @return str GET/POST/PUTなど
+ */
+function get_request_method()
+{
+    return $_SERVER['REQUEST_METHOD'];
+}
--- index.php
+++ index.php
@@ -9,22 +9,44 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 
 $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try {
-  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-  error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
-  error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
-  error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
-} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-  error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch (\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+    error_log("parseEventRequest failed. InvalidSignatureException => " . var_export($e, true));
+} catch (\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+    error_log("parseEventRequest failed. UnknownEventTypeException => " . var_export($e, true));
+} catch (\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+    error_log("parseEventRequest failed. UnknownMessageTypeException => " . var_export($e, true));
+} catch (\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+    error_log("parseEventRequest failed. InvalidEventRequestException => " . var_export($e, true));
 }
 
 foreach ($events as $event) {
-    if ($event instanceof \LINE\LINEBot\Event\PostbackEvent) {
-        // replyTextMessage($bot, $event->getReplyToken(), "Postback受信「" . $event->getPostbackData() . "」");
-        $postback = true;
-        $pbMsg = $event->getPostbackData();
+    $profile = $bot->getProfile($event->getUserId())->getJSONDecodedBody();
+    if ($event instanceof \LINE\LINEBot\Event\FollowEvent) {
+        error_log("友達追加");
+        $url = parse_url(getenv('DATABASE_URL'));
+        $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+        $pdo = new PDO($dsn, $url['user'], $url['pass']);
+
+        $sql = 'insert into public.user (user_line_id, name, comment, picture_url) values (:user_line_id, :name, :comment, :picture_url)';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(":user_line_id", $profile["userId"]);
+        $stmt->bindValue(":name", $profile["displayName"]);
+        $stmt->bindValue(":comment", $profile["statusMessage"]);
+        $stmt->bindValue(":picture_url", $profile["pictureUrl"]);
+        $flag = $stmt->execute();
+
+        replyConfirmTemplate($bot,
+            $event->getReplyToken(),
+            "友達追加",
+            "Webで詳しく見ますか？",
+            new LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder (
+                "WebReseach", "https://" . $_SERVER["HTTP_HOST"] . "/get.php?NAME=" . $profile["displayName"] . "&PIC=" . $profile["pictureUrl"])
+        );
+    }
+
+    if ($event instanceof \LINE\LINEBot\Event\UnfollowEvent) {
+        error_log("友達解除");
     }
 
     // if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
@@ -36,36 +58,36 @@ foreach ($events as $event) {
     //     continue;
     // }
 
-    $profile = $bot->getProfile($event->getUserId())->getJSONDecodedBody();
-    // $message = $profile["displayName"] . "さん、ランダムでスタンプで返答します。";
-    $message2 = "http://codezine.jp/article/detail/9905";
-//    $user_id = $profile["userId"];
-//    $displayName = $profile["displayName"];
-//    error_log($displayName);
-    // $user_info = array(
-    //     $profile["displayName"],
-    //     $profile["userId"],
-    //     $profile["pictureUrl"],
-    //     $profile["statusMessage"]
-    // );
-
-    foreach ($profile as $k => $v) {
-        error_log($k . ":" . $v);
-    }
-    error_log("update");
-
-$url = parse_url(getenv('DATABASE_URL'));
-$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-$pdo = new PDO($dsn, $url['user'], $url['pass']);
-
-$sql = 'insert into public.user (user_line_id, name, comment, picture_url) values (:user_line_id, :name, :comment, :picture_url)';
-$stmt = $pdo->prepare($sql);
-$stmt->bindValue(":user_line_id", $profile["userId"]);
-$stmt->bindValue(":name", $profile["displayName"]);
-$stmt->bindValue(":comment", $profile["statusMessage"]);
-$stmt->bindValue(":picture_url", $profile["pictureUrl"]);
-$flag = $stmt->execute();
-// 
+//    $profile = $bot->getProfile($event->getUserId())->getJSONDecodedBody();
+//    // $message = $profile["displayName"] . "さん、ランダムでスタンプで返答します。";
+//    $message2 = "http://codezine.jp/article/detail/9905";
+////    $user_id = $profile["userId"];
+////    $displayName = $profile["displayName"];
+////    error_log($displayName);
+//    // $user_info = array(
+//    //     $profile["displayName"],
+//    //     $profile["userId"],
+//    //     $profile["pictureUrl"],
+//    //     $profile["statusMessage"]
+//    // );
+//
+//    foreach ($profile as $k => $v) {
+//        error_log($k . ":" . $v);
+//    }
+//    error_log("update");
+//
+//    $url = parse_url(getenv('DATABASE_URL'));
+//    $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+//    $pdo = new PDO($dsn, $url['user'], $url['pass']);
+//
+//    $sql = 'insert into public.user (user_line_id, name, comment, picture_url) values (:user_line_id, :name, :comment, :picture_url)';
+//    $stmt = $pdo->prepare($sql);
+//    $stmt->bindValue(":user_line_id", $profile["userId"]);
+//    $stmt->bindValue(":name", $profile["displayName"]);
+//    $stmt->bindValue(":comment", $profile["statusMessage"]);
+//    $stmt->bindValue(":picture_url", $profile["pictureUrl"]);
+//    $flag = $stmt->execute();
+//
 // if ($flag){
 //    error_log('データの追加に成功しました');
 // }else{
@@ -85,39 +107,38 @@ $flag = $stmt->execute();
     // replyTextMessage($bot, $event->getReplyToken(), "TextMessage");
 //
 //    replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
-    
-    if ($postback) {
-        switch ($pbMsg) {
-            case "1_1":
-                replyTextMessage($bot, $event->getReplyToken(), "正解");
-            case "1_2":
-            case "1_3":
-            case "1_4":
-                // replyTextMessage($bot, $event->getReplyToken(), "不正解");
-                $bot->replyMessage($event->getReplyToken(),
-                (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
-                    ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("不正解、キングダム全巻を買って復習しよう！"))
-                    ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("https://www.amazon.co.jp/dp/B002DE793M/"))
-                );
-        }
-        continue;
-    }
-    
-     replyButtonsTemplate($bot,
-        $event->getReplyToken(),
-        "キングダム クイズ",
-        "https://" . $_SERVER["HTTP_HOST"] . "/imgs/1.jpg",
-        "問題",
-        "秦の怪鳥の異名を持つ六大将軍といえば？",
-        new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
-            "王騎", "1_1"),
-        new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
-            "羌瘣", "1_2"),
-        new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
-            "嬴政", "1_3"),
-        new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
-            "信", "1_4")
-     );
+
+//    if ($postback) {
+//        switch ($pbMsg) {
+//            case "1_1":
+//                replyTextMessage($bot, $event->getReplyToken(), "正解");
+//            case "1_2":
+//            case "1_3":
+//            case "1_4":
+//                $bot->replyMessage($event->getReplyToken(),
+//                    (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
+//                        ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("不正解、キングダム全巻を買って復習しよう！"))
+//                        ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("https://www.amazon.co.jp/dp/B002DE793M/"))
+//                );
+//        }
+//        continue;
+//    }
+//
+//    replyButtonsTemplate($bot,
+//        $event->getReplyToken(),
+//        "キングダム クイズ",
+//        "https://" . $_SERVER["HTTP_HOST"] . "/imgs/1.jpg",
+//        "問題",
+//        "秦の怪鳥の異名を持つ六大将軍といえば？",
+//        new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
+//            "王騎", "1_1"),
+//        new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
+//            "羌瘣", "1_2"),
+//        new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
+//            "嬴政", "1_3"),
+//        new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
+//            "信", "1_4")
+//    );
 
 //    $columnArray = array();
 //    for($i = 0; $i < 5; $i++) {
@@ -136,40 +157,44 @@ $flag = $stmt->execute();
 //        );
 //        array_push($columnArray, $column);
 //      }
-  // replyCarouselTemplate($bot, $event->getReplyToken(),"今後の天気予報", $columnArray);
+    // replyCarouselTemplate($bot, $event->getReplyToken(),"今後の天気予報", $columnArray);
 
 }
 
-function replyTextMessage($bot, $replyToken, $text) {
+function replyTextMessage($bot, $replyToken, $text)
+{
     $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
     if (!$response->isSucceeded()) {
-        error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+        error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
     }
 }
 
-function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUrl) {
+function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUrl)
+{
     $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
     if (!$response->isSucceeded()) {
-        error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+        error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
     }
 }
 
-function replyButtonsTemplate($bot, $replyToken, $alternativeText, $imageUrl, $title, $text, ...$actions) {
-  $actionArray = array();
-  foreach($actions as $value) {
-    array_push($actionArray, $value);
-  }
-  $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
-    $alternativeText,
-    new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder ($title, $text, $imageUrl, $actionArray)
-  );
-  $response = $bot->replyMessage($replyToken, $builder);
-  if (!$response->isSucceeded()) {
-    error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
-  }
+function replyButtonsTemplate($bot, $replyToken, $alternativeText, $imageUrl, $title, $text, ...$actions)
+{
+    $actionArray = array();
+    foreach ($actions as $value) {
+        array_push($actionArray, $value);
+    }
+    $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
+        $alternativeText,
+        new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder ($title, $text, $imageUrl, $actionArray)
+    );
+    $response = $bot->replyMessage($replyToken, $builder);
+    if (!$response->isSucceeded()) {
+        error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
+    }
 }
 
-function replyCarouselTemplate($bot, $replyToken, $alternativeText, $columnArray) {
+function replyCarouselTemplate($bot, $replyToken, $alternativeText, $columnArray)
+{
     $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
         $alternativeText,
         new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder (
@@ -177,9 +202,24 @@ function replyCarouselTemplate($bot, $replyToken, $alternativeText, $columnArray
     );
     $response = $bot->replyMessage($replyToken, $builder);
     if (!$response->isSucceeded()) {
-        error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+        error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
     }
 }
 
+function replyConfirmTemplate($bot, $replyToken, $alternativeText, $text, ...$actions)
+{
+    $actionArray = array();
+    foreach ($actions as $value) {
+        array_push($actionArray, $value);
+    }
+    $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
+        $alternativeText,
+        new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder ($text, $actionArray)
+    );
+    $response = $bot->replyMessage($replyToken, $builder);
+    if (!$response->isSucceeded()) {
+        error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
+    }
+}
 
 ?>
