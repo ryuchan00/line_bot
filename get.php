969ce5d9536ef8626dd7bee4e017205cc72197a7
yamakawa00 <leokun0210@gmail.com>
2017-02-03 18:53:25 +0900

    up

--- get.php
+++ get.php
@@ -12,7 +12,7 @@ $request_method = get_request_method();
 if ($request_method === 'GET') {
     foreach ($_GET as $k => $v) {
         if ($k == "PIC") {
-            echo '<img src="' . $v . '"">';
+            echo '<img src="' . $v . '""></br>';
         }
         echo "受信パラメータ" . $k . "：" . $v . "</br>";
     }
